462a0fb2ab12c74c8d18125d20aa83d8f7d24d38
Olga <Olga@NOTEBOOK_OLGA>
2020-06-10 19:51:03 +0200

    Empty Array

---
+++ lecture_6/Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        public static T[] Empty<T>()
+        {
+            return EmptyArray<T>.Value;
+        }
+       
+    }
+}
