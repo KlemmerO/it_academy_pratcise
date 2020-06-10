18fb566790d1938d5401dbbc63e5fe086e1d7ab3
Olga <Olga@NOTEBOOK_OLGA>
2020-06-10 20:06:46 +0200

    Not ended

---
+++ lecture_6/Task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[13];
+            Random rand = new Random();
+            for (int i = 0; i > 13; i++)
+                a[i] = rand.Next(1,13);
+            TextElementEnumerator.Box
+
+
+
+
+        }
+    }
+}
