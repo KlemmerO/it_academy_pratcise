2c2f48b266a646faffd9575daf5962e7401e453d
Olga <Olga@NOTEBOOK_OLGA>
2020-07-06 19:11:33 +0200

    Not resolved

---
+++ Lecture_12/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lecture_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string _NewClass;
+       uniqueltem _newClass = new Uniqueltem();
+        }
+    }
+}
