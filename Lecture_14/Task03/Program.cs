2aee74645cf7cf18a37ee68f200349719dc1e0a6
Olga <Olga@NOTEBOOK_OLGA>
2020-07-13 18:51:22 +0200

    Not Finished

---
+++ Lecture_14/Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        public static void Neighbour()
+        {
+            Console.WriteLine("Hello World!");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
